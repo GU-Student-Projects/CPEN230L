05149912dc5f316bf3ec9205aa0e4431f178e32d
gdimartino <gdimartino@gabedimartino.com>
2023-10-24 09:31:38 -0700

    updated comments

--- Lab 7/mux_display/sim/mux_disp_top_tb.v
+++ Lab 7/mux_display/sim/mux_disp_top_tb.v
@@ -13,16 +13,16 @@ module tb_oct7seg;
 
   initial begin
     SW_sim[14:0] = 15'o01234; // Use octal to deal with 3-bit groups
-    SW_sim[17:15] = 3'o0; // @t=0 output = SW[2:0] = 0 = 1000000
-    #5 SW_sim[17:15] = 3'o1; // @t=5 output = SW[2:0] = 1 = 1111001
-    #5 SW_sim[17:15] = 3'o2; // @t=10 output = SW[2:0] = 2 = 0100100
-    #5 SW_sim[17:15] = 3'o3; // @t=15 output = SW[2:0] = 3 = 0110000
-    #5 SW_sim[17:15] = 3'o4; // @t=20 output = SW[2:0] = 4 = 0011001
+    SW_sim[17:15] = 3'o0;     // @t=0 output = SW[2:0] = 0 = 1000000
+    #5 SW_sim[17:15] = 3'o1;  // @t=5 output = SW[2:0] = 1 = 1111001
+    #5 SW_sim[17:15] = 3'o2;  // @t=10 output = SW[2:0] = 2 = 0100100
+    #5 SW_sim[17:15] = 3'o3;  // @t=15 output = SW[2:0] = 3 = 0110000
+    #5 SW_sim[17:15] = 3'o4;  // @t=20 output = SW[2:0] = 4 = 0011001
     #5 SW_sim[14:0] = 15'o56777; // Set the input for 5, 6, 7
-    #5 SW_sim[17:15] = 3'o5; // @t=30 output = SW[2:0] = 5
-    #5 SW_sim[17:15] = 3'o6; // @t=35 output = SW[2:0] = 6
-    #5 SW_sim[17:15] = 3'o7; // @t=40 output = SW[2:0] = 7
-    #5 $finish; // @t=45, end simulation
+    #5 SW_sim[17:15] = 3'o5;  // @t=30 output = SW[2:0] = 5 = 0010010
+    #5 SW_sim[17:15] = 3'o6;  // @t=35 output = SW[2:0] = 6 = 0000010
+    #5 SW_sim[17:15] = 3'o7;  // @t=40 output = SW[2:0] = 7 = 1111000
+    #5 $finish;               // @t=45, end simulation
   end
 
   always begin

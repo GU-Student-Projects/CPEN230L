cf90b4999819a4f1028ee626c345ed1e023ac4f4
gdimartino <gdimartino@gabedimartino.com>
2023-10-31 14:57:25 -0700

    Lab Done

--- Lab 8/SR_Latch/src/SR_Latch.v
+++ Lab 8/SR_Latch/src/SR_Latch.v
@@ -1,11 +1,12 @@
 // CPEN 230L lab 8 part 1, SR Latch, active-low Set & Reset
 // Gabe DiMartino 10/30/2023
 
-module SR_Latch (nS, nR, Q,Qn);
-  input nS;
-  input nR;
-  output reg Q;
-  output Qn;
+module SR_Latch (
+  input nS,
+  input nR,
+  output reg Q,
+  output Qn
+);
 
   always @ (nS or nR)
   begin
